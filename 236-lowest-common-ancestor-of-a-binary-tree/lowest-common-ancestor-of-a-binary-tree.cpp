6dbc5ee2db61cbdd455577513811631b02809683
AKASH8951 <akash4602c@gmail.com>
2026-08-04 23:03:14 +0530

    Time: 70 ms (20.99%) | Memory: 43.8 MB (89.23%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,44 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    bool rootToNodePath(TreeNode* root, TreeNode* node,
-                        vector<TreeNode*>& path) {
-
-        if(root == NULL)
-            return false;
-
-        path.push_back(root);
-
-        if(root == node)
-            return true;
-
-        if(rootToNodePath(root->left, node, path) ||
-           rootToNodePath(root->right, node, path))
-            return true;
-
-        path.pop_back();
-        return false;
-    }
-
-    TreeNode* lowestCommonAncestor(TreeNode* root,
-                                   TreeNode* p,
-                                   TreeNode* q) {
-
-        vector<TreeNode*> path1, path2;
-
-        rootToNodePath(root, p, path1);
-        rootToNodePath(root, q, path2);
-
-        TreeNode* lca = nullptr;
-
-        int i = 0;
-        while(i < path1.size() && i < path2.size()) {
-            if(path1[i] != path2[i])
-                break;
-
-            lca = path1[i];
-            i++;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL){
+            return NULL;
         }
 
-        return lca;
+        if(root == p || root == q) {
+            return root;
+        }
+
+        TreeNode* leftLCA = lowestCommonAncestor(root -> left , p , q);
+        TreeNode* rightLCA = lowestCommonAncestor(root -> right , p , q);
+
+        if(leftLCA && rightLCA) 
+            return root;
+
+        return leftLCA ? leftLCA : rightLCA;
+
     }
 };
