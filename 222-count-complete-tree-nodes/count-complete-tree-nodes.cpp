5fa7ace1df665416dcd2ab4935e856523f218194
AKASH8951 <akash4602c@gmail.com>
2026-08-15 14:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (44.61%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -1,23 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    int getHeight(TreeNode* root) {
+        int h = 0;
+
+        while (root) {
+            h++;
+            root = root->left;
+        }
+
+        return h;
+    }
+
     int countNodes(TreeNode* root) {
-        if(root == NULL) {
+        if (root == NULL) {
             return 0;
         }
-        int CountLeft = countNodes(root -> left);
-        int CountRight = countNodes(root -> right);
 
-        return CountLeft + CountRight + 1;
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+
+        if (leftHeight == rightHeight) {
+            // Left subtree is perfect
+            return (1 << leftHeight) + countNodes(root->right);
+        }
+        else {
+            // Right subtree is perfect
+            return (1 << rightHeight) + countNodes(root->left);
+        }
     }
 };
